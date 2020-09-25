ea68c99ffb2e90785f3415b5255f9cbec927c805
BuildTools <unconfigured@null.spigotmc.org>
2020-09-25 16:57:59 +0200

    So

---
+++ src/tk/q11mc/Main.java
@@ -0,0 +1,30 @@
+package tk.q11mc;
+
+import com.siinus.simpleGrafixShader.ShaderProgram;
+
+public class Main extends ShaderProgram {
+
+    public static void main(String[] args) {
+        new Main().initShader();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
