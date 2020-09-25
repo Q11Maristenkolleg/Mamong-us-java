682e62d09900eca01a9072c84d03ed8e03c47556
visar77 <visarlumi77@gmail.com>
2020-09-25 17:17:42 +0200

    I made it work, unlike Simon

--- src/tk/q11mc/Main.java
+++ src/tk/q11mc/Main.java
@@ -1,30 +1,7 @@
 package tk.q11mc;
 
-import com.siinus.simpleGrafixShader.ShaderProgram;
-
-public class Main extends ShaderProgram {
-
+public class Main {
     public static void main(String[] args) {
-        new Main().initShader();
-    }
-
-    @Override
-    public void start() {
-
-    }
-
-    @Override
-    public void update() {
-
-    }
-
-    @Override
-    public void render() {
-
-    }
-
-    @Override
-    public void stop() {
-
+        System.out.println("yes");
     }
 }
