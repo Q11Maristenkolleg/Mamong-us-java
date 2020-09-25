a73ba61d3fe6be113e646e84c42cf4c90aae4e8b
BuildTools <unconfigured@null.spigotmc.org>
2020-09-25 17:32:07 +0200

    Added some stupid code

--- src/tk/q11mc/Main.java
+++ src/tk/q11mc/Main.java
@@ -1,7 +1,41 @@
 package tk.q11mc;
 
-public class Main {
+import com.siinus.simpleGrafix.gfx.Image;
+import com.siinus.simpleGrafixShader.Light;
+import com.siinus.simpleGrafixShader.ShaderProgram;
+
+public class Main extends ShaderProgram {
+    Image grassBlock = new Image("/grass.png");
+    Light light = new Light(150, 0xffffffff);
+
+    int x = 0;
+
     public static void main(String[] args) {
-        System.out.println("yes");
+        new Main().initShader();
+    }
+
+    public Main() {
+        setIconImage(grassBlock);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update() {
+        x++;
+    }
+
+    @Override
+    public void render() {
+        getRenderer().drawImage(grassBlock, x, 100);
+        getShaderRenderer().drawLight(light, getInput().getMouseX(), getInput().getMouseY());
+    }
+
+    @Override
+    public void stop() {
+
     }
 }
