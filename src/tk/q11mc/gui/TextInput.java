e6ac78ed657113e55ce36a3c32cd73187f21ff34
BuildTools <unconfigured@null.spigotmc.org>
2020-10-03 12:41:03 +0200

    Optimized text input

--- src/tk/q11mc/gui/TextInput.java
+++ src/tk/q11mc/gui/TextInput.java
@@ -11,23 +11,25 @@ public class TextInput extends GUIObject {
     private final ImageTile image;
     private final int color;
     private final Font font;
-    @Nullable private String defaultText = null;
+    @Nullable
+    private String defaultText = null;
 
-    private boolean isMouseOver =  false;
+    private boolean isMouseOver = false;
     private boolean activated = false;
     private StringBuilder text = new StringBuilder();
+    private boolean shift = false;
 
     @Nullable TextQueue queue = null;
 
     /**
      * Creates a new GUI object.
      *
-     * @param program The associated program
-     * @param sprite  The texture
-     * @param x       The offset x
-     * @param y       The offset y
-     * @param width   The width of the bounding box
-     * @param height  The width of the bounding box
+     * @param program   The associated program
+     * @param sprite    The texture
+     * @param x         The offset x
+     * @param y         The offset y
+     * @param width     The width of the bounding box
+     * @param height    The width of the bounding box
      * @param textColor The color of the text
      */
     public TextInput(Main program, ImageTile sprite, int x, int y, int width, int height, int textColor, Font font) {
@@ -43,43 +45,49 @@ public class TextInput extends GUIObject {
         if (program.getInput().isButtonDown(1)) {
             activated = isMouseOver;
         }
-        if (activated && InputUtils.isKeyTyped()) {
+        if (activated) {
             char kd = program.getInput().getLastKey();
-            if (kd=='\b') {
-                if (text.length()<=0) {
-                    return;
-                }
-                text.deleteCharAt(text.length()-1);
+            if (((byte) kd) == (byte) -1) {
+                shift = true;
                 return;
             }
-            if (kd=='\t') {
-                if (queue != null) {
-                    try {
-                        queue.fields.get(queue.fields.indexOf(this)+1).activated = true;
-                    } catch (IndexOutOfBoundsException e) {
+            if (InputUtils.isKeyTyped()){
+                if (kd == '\b') {
+                    if (text.length() <= 0) {
                         return;
                     }
-                    activated = false;
+                    text.deleteCharAt(text.length() - 1);
+                    return;
                 }
-                return;
-            }
-            if (kd=='\n') {
-                if (queue != null) {
-                    if (queue.fields.size()-1 <= queue.fields.indexOf(this)) {
-                        queue.endAction.run();
+                if (kd == '\t') {
+                    if (queue != null) {
+                        try {
+                            queue.fields.get(queue.fields.indexOf(this) + 1).activated = true;
+                        } catch (IndexOutOfBoundsException e) {
+                            return;
+                        }
                         activated = false;
                     }
+                    return;
                 }
-                return;
+                if (kd == '\n') {
+                    if (queue != null) {
+                        if (queue.fields.size() - 1 <= queue.fields.indexOf(this)) {
+                            queue.endAction.run();
+                            activated = false;
+                        }
+                    }
+                    return;
+                }
+                text.append(shift ? String.valueOf(kd) : (String.valueOf(kd).toLowerCase()));
             }
-            text.append((InputUtils.isKeyPressed(0x10))?String.valueOf(kd):(String.valueOf(kd).toLowerCase()));
         }
     }
 
     @Override
     public void render() {
-        program.getRenderer().drawImageTile(image, x, y, 0, activated?2:(isMouseOver?1:0));
-        program.getRenderer().drawText(text.length()<=0?(defaultText==null?"":defaultText):text.toString(), x + 10, y + 10, text.length()<=0?0xff7f7f7f:color, font);
+        program.getRenderer().drawImageTile(image, x, y, 0, activated ? 2 : (isMouseOver ? 1 : 0));
+        program.getRenderer().drawText(text.length() <= 0 ? (defaultText == null ? "" : defaultText) : text.toString(), x + 10, y + 10, text.length() <= 0 ? 0xff7f7f7f : color, font);
     }
 
     public String getText() {
