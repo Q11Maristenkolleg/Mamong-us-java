d7908a07332bfd3272efe2cf3428684d882393b9
BuildTools <unconfigured@null.spigotmc.org>
2020-10-03 20:06:55 +0200

    Made player animation

--- src/tk/q11mc/net/Protocol.java
+++ src/tk/q11mc/net/Protocol.java
@@ -17,7 +17,7 @@ public class Protocol implements ClientHandler {
     }
 
     @Override
-    public String processInput(@NotNull String s) {
+    public synchronized String processInput(@NotNull String s) {
         String[] msg = s.split(" ");
         String ip = msg[0];
         if (msg.length<2) {
@@ -45,9 +45,20 @@ public class Protocol implements ClientHandler {
                     }
                 }
                 case "pos" -> {
-                    if (!Multiplayer.ip.equals(ip) && msg.length >= 4) {
+                    if (Multiplayer.ip!=null && !Multiplayer.ip.equals(ip) && msg.length >= 4) {
+                        Multiplayer.players.get(ip).frameUp();
                         Multiplayer.players.get(ip).setX(Integer.parseInt(msg[2]));
                         Multiplayer.players.get(ip).setY(Integer.parseInt(msg[3]));
+                    }
+                }
+                case "move" -> {
+                    if (Multiplayer.ip!=null && !Multiplayer.ip.equals(ip) && msg.length >= 3) {
+                        Multiplayer.players.get(ip).setMoving(msg[2].equals("start"));
+                    }
+                }
+                case "face" -> {
+                    if (Multiplayer.ip!=null && !Multiplayer.ip.equals(ip) && msg.length >= 3) {
+                        Multiplayer.players.get(ip).setLeft(msg[2].equals("left"));
                     }
                 }
             }
