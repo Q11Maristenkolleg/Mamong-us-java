91dd621e50eac3543bdb0f13298f42029e57d27e
visar77 <visarlumi77@gmail.com>
2020-10-04 11:11:10 +0200

    Improved Camera

--- src/tk/q11mc/core/Camera.java
+++ src/tk/q11mc/core/Camera.java
@@ -5,18 +5,19 @@ import tk.q11mc.objects.Player;
 public class Camera {
     private final Player player;
     private int x, y;
-    private final int width = 100, height = 100;
 
     public Camera(Player player) {
         this.player = player;
     }
 
     public void update() {
-        int targetX = player.getX() - width/2 + player.getWidth()/2;
-        int targetY = player.getY() - height/2 + player.getHeight()/2;
+        int width = 100;
+        int targetX = player.getX() - width /2 + player.getWidth()/2;
+        int height = 100;
+        int targetY = player.getY() - height /2 + player.getHeight()/2;
 
         targetX = Math.min(/*Map.width*/ 5000 - width, Math.max(0, targetX));
-        targetY = Math.min(/*Map.height*/2000 -height, Math.max(0, targetY));
+        targetY = Math.min(/*Map.height*/2000 - height, Math.max(0, targetY));
 
         x += (targetX - x) * 0.1;
         y += (targetY - y) * 0.1;
